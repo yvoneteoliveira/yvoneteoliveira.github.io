fe900f61a89d78ca3d7e316402e806ba3bcb492c
yvoneteoliveira <yvoneteoliveira@gmail.com>
2020-01-09 10:37:58 +0000

    alterei home e banner

---
+++ minimalistafrontend/Nova pasta/transicao.js
@@ -0,0 +1,12 @@
+//var hide =  document.getElementById("programmer").style.display = "none";
+/* function hideElement() {
+} */
+
+//document.getElementById("navbar-toggler").addEventListener("click", hideElement, true);
+
+
+
+
+
+
+
